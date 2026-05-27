a3d11f89e6b3c401d81bf1d2244151fc34b00df4
Daniel Sweetser <dmsweetser@gmail.com>
2026-05-26 20:50:00 -0400

    Cleanup / bug fix

--- walbert/__init__.py
+++ walbert/__init__.py
@@ -8,14 +8,12 @@ from .io.console import ConsoleIOLayer
 from .io.serial import SerialIOLayer
 from .models.manager import ModelManager
 from .database.manager import DatabaseManager
-from .response.parser import ResponseParser
 from .authorization.manager import AuthorizationManager
 from .agent import WalbertAgent
 
 __all__ = [
     'Config', 'IOConfig',
     'IOLayerFactory', 'ConsoleIOLayer', 'SerialIOLayer',
-    'ModelManager', 'DatabaseManager',
-    'ResponseParser', 'AuthorizationManager',
+    'ModelManager', 'DatabaseManager', 'AuthorizationManager',
     'WalbertAgent'
 ]
