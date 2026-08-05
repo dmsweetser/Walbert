17a44fccb353e9f5f8f73d3794b4d2ab841fc0e6
Daniel Sweetser <dmsweetser@gmail.com>
2026-08-05 04:51:53 -0400

    Bug fix

--- _install.sh
+++ _install.sh
@@ -138,8 +138,7 @@ cat > instance/config.json << EOF
     "autonomous_operation_timeout": 120,
     "conversation_log_dir": "instance/conversations",
     "database_path": "instance/walbert.db",
-    "be_presbyterian": true,
-    "max_context_blocks": 20
+    "be_presbyterian": true
 }
 EOF
 
