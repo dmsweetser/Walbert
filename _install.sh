2ac796f0c879fbac63d3eedde57e9c1356c70911
Daniel Sweetser <dmsweetser@gmail.com>
2026-08-12 08:01:49 -0400

    Bug fix

--- _install.sh
+++ _install.sh
@@ -5,10 +5,10 @@ set -e
 
 echo "Setting up Walbert..."
 
-sudo apt update
 sudo apt install build-essential libssl-dev zlib1g-dev \
                  libbz2-dev libreadline-dev libsqlite3-dev \
                  libffi-dev liblzma-dev libudev-dev
+sudo apt install python3.11-dev
 sudo apt install libbluetooth-dev
 
 # Create directories
