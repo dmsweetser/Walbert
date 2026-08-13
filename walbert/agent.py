ea3080a3d07d49d997fbf974a1fa1348c77e08ee
Daniel Sweetser <dmsweetser@gmail.com>
2026-08-13 08:08:12 -0400

    Format fix

--- walbert/agent.py
+++ walbert/agent.py
@@ -414,7 +414,7 @@ Error: {str(e)}
             self.comms = NetworkManager(self.config)
         self.comms.start()
         self._comms_started = True
-        self.logger.info("Peer communication enabled")
+        self.logger.info(f"{chr(10)}Peer communication enabled{chr(10)}{chr(10)}")
 
     def disable_peer_communication(self):
         self.config.peer_communication_enabled = False
@@ -423,7 +423,7 @@ Error: {str(e)}
         if self.comms:
             self.comms.stop()
         self._comms_started = False
-        self.logger.info("Peer communication disabled")
+        self.logger.info(f"{chr(10)}Peer communication disabled{chr(10)}{chr(10)}")
 
     def enable_audio(self):
         self.config.audio_enabled = True
