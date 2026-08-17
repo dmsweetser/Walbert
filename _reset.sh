fab371ce865e1a3ae345b9d439e03a448e993905
Daniel Sweetser <dmsweetser@gmail.com>
2026-08-17 06:46:12 -0400

    Cleanup and prompt improvements

--- _reset.sh
+++ _reset.sh
@@ -1,5 +1,7 @@
 rm -r ./instance/config.json 
 rm -r ./instance/conversations/*
 rm -r ./instance/walbert.*
+rm -r ./walbert_prompt_*
+rm -r ./walbert_response_*
 chmod +x ./_install.sh
 ./_install.sh
