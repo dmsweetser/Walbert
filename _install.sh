d6f7f90c7a93309a3412969a7ef802d47c27caf3
Daniel Sweetser <dmsweetser@gmail.com>
2026-08-27 06:55:15 -0400

    More fixes

--- _install.sh
+++ _install.sh
@@ -13,6 +13,7 @@ sudo apt install -y portaudio19-dev
 sudo apt install -y libbluetooth-dev
 
 sudo apt install pulseaudio
+sudo apt install espeak-ng  # Debian/Ubuntu
 
 # PipeWire + Bluetooth
 sudo apt install -y pipewire-audio-client-libraries libspa-0.2-bluetooth
