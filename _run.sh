d3ac1d4bd0278cc6d4d99d6852057b9e44d4141b
Daniel Sweetser <dmsweetser@gmail.com>
2026-09-04 07:35:26 -0400

    More sudo fix

--- _run.sh
+++ _run.sh
@@ -7,7 +7,7 @@ export ALSA_CONFIG_PATH=/dev/null
 
 source venv/bin/activate
 if [ "$1" = "test" ]; then
-    sudo python3 -m unittest discover -v
+    sudo -E python3 -m unittest discover -v
 else
-    sudo python3 main.py
+    sudo -E python3 main.py
 fi
